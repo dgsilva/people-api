90e4383791cee43a37fce36627ab7fb66f768502
Diego Bizerra da Silva <diegorj93@gmail.com>
2021-06-25 10:28:32 -0300

    Implementado metodo listar

--- personapi/src/main/java/br/person/api/controller/PersonController.java
+++ personapi/src/main/java/br/person/api/controller/PersonController.java
@@ -1,6 +1,9 @@
 package br.person.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +19,10 @@ public class PersonController {
 	@Autowired
 	private PersonRepository personRepository;
 	
+	@GetMapping
+	public List<Person>findAll(){
+		return personRepository.findAll();
+	}
 	@PostMapping
 	public Person create(@RequestBody Person person) {
 		Person personSave = personRepository.save(person);
