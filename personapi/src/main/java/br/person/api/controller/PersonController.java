6b6ae358c98c88b3ba1671947e49a968edef1a1c
Diego Bizerra da Silva <diegorj93@gmail.com>
2021-06-30 11:11:08 -0300

    Deveopep person create operation

--- personapi/src/main/java/br/person/api/controller/PersonController.java
+++ personapi/src/main/java/br/person/api/controller/PersonController.java
@@ -3,14 +3,18 @@ package br.person.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.person.api.dto.MessageResponseDTO;
 import br.person.api.entity.Person;
 import br.person.api.repository.PersonRepository;
+import br.person.api.service.PersonService;
 
 @RestController
 @RequestMapping("persons")
@@ -18,14 +22,17 @@ public class PersonController {
 
 	@Autowired
 	private PersonRepository personRepository;
+	@Autowired
+	private PersonService PersonService;
 	
 	@GetMapping
 	public List<Person>findAll(){
 		return personRepository.findAll();
 	}
+	
 	@PostMapping
-	public Person create(@RequestBody Person person) {
-		Person personSave = personRepository.save(person);
-		return personSave;
+	@ResponseStatus(HttpStatus.CREATED)
+	public MessageResponseDTO create(@RequestBody Person person) {
+		return  PersonService.create(person);
 	}
 }
--- personapi/src/main/java/br/person/api/service/PersonService.java
+++ personapi/src/main/java/br/person/api/service/PersonService.java
@@ -1,5 +1,26 @@
 package br.person.api.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import br.person.api.dto.MessageResponseDTO;
+import br.person.api.entity.Person;
+import br.person.api.repository.PersonRepository;
+
+@Service
 public class PersonService {
 
+	@Autowired
+	private PersonRepository personRepository;
+	
+	
+	public MessageResponseDTO create(@RequestBody Person person) {
+		Person personSave = personRepository.save(person);
+		return MessageResponseDTO
+				.builder()
+				.message("Seu cadastro foi realizado " + personSave.getId())
+				.build();
+	}
+	
+	
 }
